7c5b60e34b7a702345070e8d1182eec12703a0d6
Sharif Elfouly <selfouly@gmail.com>
2020-07-26 15:54:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt install -y nload htop vim docker.io tmux python3-pip nvidia-cuda-toolkit python3-pip
-pip3 install torch
+pip3 install torch Pillow opencv-python dirly
 snap install jupyter
 ubuntu-drivers autoinstall
 reboot
