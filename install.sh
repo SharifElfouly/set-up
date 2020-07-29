1fd187081a40d1f54aed5ad554251f2c075a85cb
Sharif Elfouly <selfouly@gmail.com>
2020-07-29 21:50:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@ apt install -y nload htop vim docker.io tmux python3-pip nvidia-cuda-toolkit pyt
 pip3 install torch Pillow opencv-python dirly
 snap install jupyter
 snap install code --classic
+
+# vim stuff
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+sh ln_dotfiles.sh
 ubuntu-drivers autoinstall
 reboot
