ef4dd1b469adf15d8d198747c276170adbeeaf61
Sharif Elfouly <selfouly@gmail.com>
2020-10-23 16:53:55 +0200

    nvidia-docker

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@ pip3 install torch Pillow opencv-python dirly
 snap install jupyter
 snap install code --classic
 
+# nvidia-docker2
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+
 # vim stuff
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
