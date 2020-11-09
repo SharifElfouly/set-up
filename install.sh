8112e747f9f2fc452451fbe8c559e6559cb7edb7
SharifElfouly <selfouly@gmail.com>
2020-11-09 20:04:32 +0100

    nvtop

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install -y nload htop vim docker.io tmux python3-pip nvidia-cuda-toolkit python3-pip libgnome2-bin gnome-screensaver curl
+apt install -y nload htop vim docker.io tmux python3-pip nvidia-cuda-toolkit python3-pip libgnome2-bin gnome-screensaver curl nvtop
 pip3 install torch Pillow opencv-python dirly
 snap install jupyter
 snap install code --classic
